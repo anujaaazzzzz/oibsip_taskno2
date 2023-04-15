137604ded945365fa2cfa00d867b6d3bea5f1863
ANUJA <119028120+anujaaazzzzz@users.noreply.github.com>
2023-04-15 14:41:19 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function submitForm() {
+    // Clear input fields
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("address").value = "";
+    document.getElementById("comment").value = "";
+}
